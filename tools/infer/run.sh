007c7517a367108d8c7223e4cc9fcf71b40a4283
Yu Li <lyo.gavin@gmail.com>
2022-09-23 08:40:17 -0500

    runsh

--- tools/infer/run.sh
+++ tools/infer/run.sh
@@ -1,4 +1,5 @@
 nohup python tools/infer/predict_system.py --use_gpu=True --use_onnx=True --det_model_dir=./inference/det_onnx/model.onnx \
  --rec_model_dir=./inference/rec_onnx/model.onnx  --cls_model_dir=./inference/cls_onnx/model.onnx \
- --image_dir=/home/ubuntu/cloudfs/ghost_data/newred_redbook_link_download/downloaded_images \
- --draw_img_save_dir=/home/ubuntu/cloudfs/ghost_data/newred_redbook_link_download/downloaded_images/ocr_results &
+ --image_dir=/home/ubuntu/cloudfs/ghost_data/newred_redbook_link_download/downloaded_images,/home/ubuntu/cloudfs/ghost_data/newred_redbook_link_download/downloaded_images/aiwriter-fe003/downloaded_images \
+ --draw_img_save_dir=/home/ubuntu/cloudfs/ghost_data/newred_redbook_link_download/downloaded_images/ocr_results \
+ --use_mp=True --total_process_num=7 &
