6fb895dbc330a6e7382361a5a036f954e5a33492
Yu Li <lyo.gavin@gmail.com>
2022-09-25 21:36:36 -0500

    img list

--- tools/infer/predict_system.py
+++ tools/infer/predict_system.py
@@ -45,12 +45,36 @@ import tools.infer.utility as utility
 import tools.infer.predict_rec as predict_rec
 import tools.infer.predict_det as predict_det
 import tools.infer.predict_cls as predict_cls
-from ppocr.utils.utility import get_image_file_list, check_and_read
+#from ppocr.utils.utility import get_image_file_list, check_and_read
+from ppocr.utils.utility import check_and_read
 
 from tools.infer.utility import draw_ocr_box_txt, get_rotate_crop_image
 import datadog_sender
 
 
+
+def _check_image_file(path):
+    img_end = {'jpg', 'bmp', 'png', 'jpeg', 'rgb', 'tif', 'tiff', 'gif', 'pdf', 'jpe'}
+    return any([path.lower().endswith(e) for e in img_end])
+
+
+def get_image_file_list(img_file):
+    imgs_lists = []
+    if img_file is None or not os.path.exists(img_file):
+        raise Exception("not found any img file in {}".format(img_file))
+
+    img_end = {'jpg', 'bmp', 'png', 'jpeg', 'rgb', 'tif', 'tiff', 'gif', 'pdf', 'jpe'}
+    if os.path.isfile(img_file) and _check_image_file(img_file):
+        imgs_lists.append(img_file)
+    #elif os.path.isdir(img_file):
+    #    for single_file in os.listdir(img_file):
+    #        file_path = os.path.join(img_file, single_file)
+    #        if os.path.isfile(file_path) and _check_image_file(file_path):
+    #            imgs_lists.append(file_path)
+    if len(imgs_lists) == 0:
+        raise Exception("not found any img file in {}".format(img_file))
+    imgs_lists = sorted(imgs_lists)
+    return imgs_lists
 
 class TextSystem(object):
     def __init__(self, args):
